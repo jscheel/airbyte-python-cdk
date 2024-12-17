3186aec1d7b01406810b2b809903c873251a0d0d
maxi297 <maxime@airbyte.io>
2024-12-17 10:44:43 -0500

    [WIP] brainstorm

--- airbyte_cdk/sources/declarative/decoders/decoder.py
+++ airbyte_cdk/sources/declarative/decoders/decoder.py
@@ -30,3 +30,44 @@ class Decoder:
         :param response: the response to decode
         :return: Generator of Mapping describing the response
         """
+
+
+class ParserSelector:
+    def select(self, response) -> Parser:
+        match response.header["Content-type"]:
+            case "application/json":
+                return response.json()
+            case "ijasdifjdasif"
+
+
+class Parser(ABC):
+    def parser(self, bytes) -> Generator[MutableMapping[str, Any], None, None]:
+        pass
+
+class GenericParserDecoder(Decoder):
+
+    def is_stream_response(self) -> bool:
+        pass
+
+    def decode(self, response: requests.Response) -> Generator[MutableMapping[str, Any], None, None]:
+        parser = self._parser_selector.select(response)
+        return parser.parse(response.content)
+
+
+class GzipParser(Parser):
+    def __init__(self, other_parser):
+        self._other_parser =other_parser
+
+    def parse(self, bytes) -> Generator[MutableMapping[str, Any], None, None]:
+        decoded_response = # do the gzip part
+        return other_parser.parse(decoded_response)
+
+
+class ZipParser(Parser):
+    def __init__(self, other_parser: Parser):
+        self._other_parser = other_parser  # it can be GzipParser or JsonlParser
+
+    def parse(self, bytes) -> Generator[MutableMapping[str, Any], None, None]:
+        # do the zip part
+        file_from_zip = get_the_file_content
+        return other_parser.parse(file_from_zip)
