f2d644f472b0f391df458d1fc36c7c7f9d0ccaee
ChristoGrab <christo.grab@gmail.com>
2025-01-03 14:23:35 -0800

    feat: add field_path attribute to Requestoption

--- airbyte_cdk/sources/declarative/requesters/request_option.py
+++ airbyte_cdk/sources/declarative/requesters/request_option.py
@@ -4,7 +4,7 @@
 
 from dataclasses import InitVar, dataclass
 from enum import Enum
-from typing import Any, Mapping, Union
+from typing import Any, Literal, List, Mapping, Optional, Union
 
 from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
 
@@ -21,18 +21,49 @@ class RequestOptionType(Enum):
 
 
 @dataclass
+class FieldName:
+    """Represents a direct field name reference"""
+    value: Union[InterpolatedString, str]
+    type: Literal["field_name"] = "field_name"
+
+@dataclass
+class FieldPath:
+    """Represents a path to a nested field"""
+    value: List[Union[InterpolatedString, str]]
+    type: Literal["field_path"] = "field_path"
+
+@dataclass
 class RequestOption:
     """
     Describes an option to set on a request
-
+    
     Attributes:
-        field_name (str): Describes the name of the parameter to inject
-        inject_into (RequestOptionType): Describes where in the HTTP request to inject the parameter
+        field_name: Describes the name of the parameter to inject. Mutually exclusive with field_path.
+        field_path: Describes the path to a nested field as a list of field names. Mutually exclusive with field_name.
+        inject_into: Describes where in the HTTP request to inject the parameter
     """
-
-    field_name: Union[InterpolatedString, str]
     inject_into: RequestOptionType
     parameters: InitVar[Mapping[str, Any]]
+    field_name: Optional[Union[InterpolatedString, str]] = None
+    field_path: Optional[List[Union[InterpolatedString, str]]] = None
 
     def __post_init__(self, parameters: Mapping[str, Any]) -> None:
-        self.field_name = InterpolatedString.create(self.field_name, parameters=parameters)
+        # Validate inputs
+        if self.field_name is None and self.field_path is None:
+            raise ValueError("RequestOption requires either a field_name or field_path")
+        if self.field_name is not None and self.field_path is not None:
+            raise ValueError("Only one of field_name or field_path can be provided to RequestOption")
+        
+        # Handle interpolation
+        if self.field_name is not None:
+            self.field_name = InterpolatedString.create(self.field_name, parameters=parameters)
+        if self.field_path is not None:
+            self.field_path = [
+                InterpolatedString.create(segment, parameters=parameters)
+                for segment in self.field_path
+            ]
+
+    @property
+    def is_field_path(self) -> bool:
+        """Returns whether this option uses a field path"""
+        return self.field_path is not None
